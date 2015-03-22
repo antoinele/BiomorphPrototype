fbfa49f5dc4f2e74b36633589d9bf2f07e6d5dd9
Joshan <Joshan@192.168.0.4>
2015-03-22 16:45:14 +0000

    Gallery Class Updates

--- src/main/java/aston/group2/biomorph/GUI/Gallery.java
+++ src/main/java/aston/group2/biomorph/GUI/Gallery.java
@@ -32,32 +32,77 @@ public class Gallery extends JFrame{
 
 	private JPanel panelOne;
 	private JPanel panelTwo;
+	private JPanel middlePanel;
+	private JButton biomorph1;
+	private JButton biomorph2;
+	private JButton biomorph3;
+	private JButton biomorph4;
+	private JButton biomorph5;
+	private JButton biomorph6;
+	
+	
 	private JButton back;
 	private JButton exit;
 	private JButton compareBiomorphs;
 	private JButton save;
 	
 	
+	private final int fixedWidth = 800;
+	private final int fixedHeight = 600;
+	private int newWidth = 0;
+	private int newHeight = 0;
+	
 	
 	public Gallery(){
+		
 		setMinimumSize(new Dimension(400,400));
 		panelOne = new JPanel();
 		panelOne.setLayout(new FlowLayout());
 		panelTwo = new JPanel();
 		panelTwo.setLayout(new FlowLayout());
+		middlePanel = new JPanel();
+		middlePanel.setLayout(new FlowLayout());
 		back = new JButton("Back");
 		exit = new JButton("Exit");
 		save = new JButton("Save/Export");
 		compareBiomorphs = new JButton("Compare Biomorphs");
+		biomorph1 = new JButton();
+		biomorph2 = new JButton();
+		biomorph3 = new JButton();
+		biomorph4 = new JButton();
+		biomorph5 = new JButton();
+		biomorph6 = new JButton();
 		
 		
+		biomorph1.setPreferredSize(new Dimension(100, 100));
+	    biomorph2.setPreferredSize(new Dimension(100, 100));
+	    biomorph3.setPreferredSize(new Dimension(100, 100));
+	    biomorph4.setPreferredSize(new Dimension(100, 100));
+	    biomorph5.setPreferredSize(new Dimension(100, 100));
+	    biomorph6.setPreferredSize(new Dimension(100, 100));
+	    
+	    
+	    
+	    middlePanel.add(biomorph1);
+	    middlePanel.add(biomorph2);
+	    middlePanel.add(biomorph3);
+	    middlePanel.add(biomorph4);
+	    middlePanel.add(biomorph5);
+	    middlePanel.add(biomorph6);
+	    
+		
+	    
+	    
 		panelOne.add(compareBiomorphs);
 		panelOne.add(save);
 		panelTwo.add(back);
 		panelTwo.add(exit);
 		
-		 add(panelOne, BorderLayout.SOUTH);
+
 		 add(panelTwo, BorderLayout.NORTH);
+		 add(middlePanel, BorderLayout.CENTER);
+		 add(panelOne, BorderLayout.SOUTH);
+		
 		
 		
 	}
