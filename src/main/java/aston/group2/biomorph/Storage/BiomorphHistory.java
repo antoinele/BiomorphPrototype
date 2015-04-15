1947484e81ff85ac76a4a9cfe2daf8f920f3ea8b
Theo Matthews <theo.matthews@hotmail.co.uk>
2015-04-15 15:21:30 +0100

    Created class selected Biomorph and added to BiomorphHistory

--- src/main/java/aston/group2/biomorph/Storage/BiomorphHistory.java
+++ src/main/java/aston/group2/biomorph/Storage/BiomorphHistory.java
@@ -1,8 +1,16 @@
 package aston.group2.biomorph.Storage;
+import java.util.ArrayList;
+import java.util.List;
 
+import aston.group2.biomorph.Model.*;
 /**
  * Created by antoine on 12/03/15.
  */
 public class BiomorphHistory {
-
+	private List<Species> biomorphList;
+	
+	public BiomorphHistory(){
+		List<Species> biomorphList = new ArrayList<Species>();
+		
+	}
 }
---
+++ src/main/java/aston/group2/biomorph/Storage/SelectedBiomorphs.java
@@ -0,0 +1,18 @@
+package aston.group2.biomorph.Storage;
+
+import java.util.List;
+import aston.group2.biomorph.Model.*;
+import aston.group2.biomorph.Model.Species;
+
+//class used for storing biomorphs that have been selected
+
+public class SelectedBiomorphs {
+	private List<Biomorph> selectedList;
+	
+	public SelectedBiomorphs(){
+		selectedList = new java.util.LinkedList();
+		
+		
+	}
+	
+}
