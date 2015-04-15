c3efa17b6842586bee3ea6de6a1d12b814236a81
Antoine Le <antoine@antoine.me.uk>
2015-04-15 17:09:24 +0100

    Still working on merging

--- src/main/java/aston/group2/biomorph/Model/Mutator.java
+++ src/main/java/aston/group2/biomorph/Model/Mutator.java
@@ -12,50 +12,97 @@ public class Mutator {
     public int childrenRequired;
     public Random random;
 
-    private static Gene mergeGenes(Gene rootGene1, Gene rootGene2, Random rng)
+    private static Gene mergeGene(Gene g1, Gene g2, Random rng)
     {
-        short[] rgv1 = rootGene1.getValues(),
-                rgv2 = rootGene2.getValues();
+        short[] v1 = g1.getValues(),
+                v2 = g2.getValues();
 
         //Merge values together
 
-        short[] newValues = new short[Math.max(rgv1.length,rgv2.length)];
+        short[] newValues = new short[Math.max(v1.length,v2.length)];
         int i=0;
-        for(; i < Math.min(rgv1.length,rgv2.length); i++)
+        for(; i < Math.min(v1.length,v2.length); i++)
         {
-            newValues[i] = (short)((rgv1[i] + rgv2[i]) / 2);
+            newValues[i] = (short)((v1[i] + v2[i]) / 2);
         }
 
         for (; i < newValues.length; i++)
         {
-            if(rgv1.length > rgv2.length) {
-                newValues[i] = rgv1[i];
+            if(v1.length > v2.length) {
+                newValues[i] = v1[i];
             }
             else
             {
-                newValues[i] = rgv2[i];
+                newValues[i] = v2[i];
             }
         }
 
         //Determine gene type
         char geneCode;
-        if(rootGene1.getGeneCode() != rootGene2.getGeneCode())
+        if(g1.getGeneCode() != g2.getGeneCode())
         {
             //Flip a coin?
             if(rng.nextBoolean())
-                geneCode = rootGene1.getGeneCode();
+                geneCode = g1.getGeneCode();
             else
-                geneCode = rootGene2.getGeneCode();
+                geneCode = g2.getGeneCode();
         }
         else
         {
-            geneCode = rootGene1.getGeneCode();
+            geneCode = g1.getGeneCode();
         }
 
         Gene newGene = GeneFactory.getGeneFromCode(geneCode);
         newGene.setValues(newValues);
 
         return newGene;
+    }
+
+    private static Gene mergeGenes(Gene rootGene1, Gene rootGene2, Random rng)
+    {
+        Gene[] sg1 = rootGene1.getSubGenes(),
+               sg2 = rootGene2.getSubGenes();
+
+        Gene[] newGene = new Gene[Math.max(sg1.length,sg2.length)];
+
+        int i=0;
+        for (; i<Math.min(sg1.length,sg2.length); i++)
+        {
+            newGene[i] = mergeGene(sg1[i], sg2[i], rng);
+
+            if(rootGene1.getSubGenes().length > 0 || rootGene2.getSubGenes().length > 0)
+            {
+                Gene newSubGene = mergeGenes(sg1[i], sg2[i], rng);
+                for(Gene nsg : newSubGene.getSubGenes())
+                    newGene[i].addSubGene(nsg);
+            }
+        }
+
+        for (; i<Math.max(sg1.length,sg2.length); i++)
+        {   // TODO: figure out how to merge additional genes
+            if(sg1.length > sg2.length)
+            {
+                newGene[i] = sg1[i];
+            }
+            else
+            {
+                newGene[i] = sg2[i];
+            }
+        }
+
+
+    }
+
+    private static Gene mergeGenes(Gene[] genes, Random rng)
+    {
+        Gene mergedGenes = genes[0];
+
+        for(int i=1; i<genes.length; i++)
+        {
+            mergedGenes = mergeGenes(mergedGenes, genes[i], rng);
+        }
+
+        return mergedGenes;
     }
 
     private static Gene mergeGenes(Biomorph[] biomorphs, Random rng)
@@ -70,9 +117,9 @@ public class Mutator {
         return mergedGenes;
     }
 
-    private Gene mutateGenes(Gene rootGene1, Gene rootGene2, Random rng)
+    private static Gene mutateGenes(Gene rootGene, Random rng)
     {
-        return null;
+
     }
 
     public Generation mutateBiomorph(Biomorph[] biomorph, Gene[] protectedParts) {
@@ -88,12 +135,21 @@ public class Mutator {
         newGeneration.children = new Biomorph[childrenRequired];
         newGeneration.parents = biomorph;
 
-        Gene mergedGenes = mergeGenes(biomorph, random);
-
         for (int i=0; i<childrenRequired; i++)
         {
             Biomorph newBiomorph = new Biomorph(newGeneration);
 
+            Gene newGenes = mergeGenes(biomorph, random);
+            newGenes = mutateGenes(newGenes, random);
+
+            //set genes
+            Gene rg = newBiomorph.getRootGene();
+            for (Gene gene : newGenes.getSubGenes())
+            {
+                rg.addSubGene(gene);
+            }
+
+            newGeneration.children[i] = newBiomorph;
         }
 
         return newGeneration;
