d4a4d58fa060bc466eed5a2ba8100edaa5e147e8
Theo Matthews <theo.matthews@hotmail.co.uk>
2015-04-15 17:39:41 +0100

    Made biomorph surface with tools test jpanel

--- src/main/java/aston/group2/biomorph/GUI/BiomorphSurfaceWithTools.java
+++ src/main/java/aston/group2/biomorph/GUI/BiomorphSurfaceWithTools.java
@@ -1,5 +1,14 @@
 package aston.group2.biomorph.GUI;
 
-public class BiomorphSurfaceWithTools {
+import javax.swing.JPanel;
 
+import aston.group2.biomorph.Model.Biomorph;
+
+public class BiomorphSurfaceWithTools extends JPanel {
+	public BiomorphSurfaceWithTools(){
+		BiomorphSurface bS = new BiomorphSurface();
+		this.add(bS);
+		bS.setBiomorph(new Biomorph("D21F00CSLBEEF00SMCAFEsL123456LFF12F0SLF24300s"));
+		
+	}
 }
