ad0dd809e4ad5ba081ad8f64d0b51045b6fc1576
Theo Matthews <theo.matthews@hotmail.co.uk>
2015-04-15 14:43:32 +0100

    Hall of Fame name change

---
+++ src/main/java/aston/group2/biomorph/Storage/HallOfFame.java
@@ -0,0 +1,18 @@
+package aston.group2.biomorph.Storage;
+
+import aston.group2.biomorph.Model.Biomorph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by antoine on 12/03/15.
+ */
+public class HallOfFame {
+    private List<Biomorph> favouriteBiomorphs;
+
+    public HallOfFame()
+    {
+        favouriteBiomorphs = new ArrayList<Biomorph>();
+    }
+}
