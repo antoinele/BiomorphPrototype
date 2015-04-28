7055133b6c9b827296264f17e874f8ddb6944176
grewalj <Joshan@192.168.0.4>
2015-04-28 22:40:20 +0100

    Settings Functionality Added

--- src/main/java/aston/group2/biomorph/GUI/MutationWindow.java
+++ src/main/java/aston/group2/biomorph/GUI/MutationWindow.java
@@ -11,6 +11,9 @@ import javax.imageio.ImageIO;
 import javax.swing.*;
 import javax.swing.border.Border;
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.*;
 import java.awt.event.ActionEvent;
@@ -18,8 +21,12 @@ import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowListener;
 import java.awt.image.BufferedImage;
+import java.io.BufferedWriter;
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 
 /**
@@ -31,6 +38,16 @@ public class MutationWindow extends JFrame {
 	Mutator mutator;
 	private JPanel hofPanel;
 	private JPanel topOfPage;
+	private JFrame frame;
+	private JPanel mainPanel;
+	
+	private int totalBiomorphs;
+	private JSlider numberOfBios;
+	private JSlider slider1;
+	private JSlider slider2;
+	private JLabel numOfBios;
+	private JLabel slider1l;
+	private JLabel slider2l;
 
     private final int hofEntries = 10;
 
@@ -53,8 +70,8 @@ public class MutationWindow extends JFrame {
 
 
 	public MutationWindow(int numberOfBiomorphs) {
-		
-		this.cols = numberOfBiomorphs / rows;
+		totalBiomorphs = numberOfBiomorphs;
+		this.cols = totalBiomorphs / rows;
 
 		setLayout(new BorderLayout());
 		setTitle("Mutation Tester");
@@ -86,9 +103,95 @@ public class MutationWindow extends JFrame {
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
 		footer.add(buttonPanel, BorderLayout.EAST);
 		
-		buttonPanel.add(makeButton("table_refresh", "Refresh"));
-		buttonPanel.add(makeButton("arrow_left", "Undo"));
-		buttonPanel.add(makeButton("table_gear", "Settings"));
+		{
+			JButton refresh, undo, settings;
+			
+			buttonPanel.add(refresh = makeButton("table_refresh", "Refresh"));
+			buttonPanel.add(undo = makeButton("arrow_left", "Undo"));
+			buttonPanel.add(settings = makeButton("table_gear", "Settings"));
+			
+			settings.addActionListener(new ActionListener() {
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					frame = new JFrame("Settings");
+					mainPanel = new JPanel();
+					
+					
+					
+					frame.pack();
+					frame.setVisible(true);
+					frame.setMinimumSize(new Dimension(380, 340));
+					frame.setResizable(false);
+					
+					mainPanel.setLayout(null);
+					
+					
+					frame.add(mainPanel, BorderLayout.CENTER);
+					
+					
+					numOfBios = new JLabel("Number of Biomorphs " + 2);
+					numberOfBios = new JSlider(JSlider.HORIZONTAL, 2, 10, 2);
+					numberOfBios.setValue(2);
+					numberOfBios.setMajorTickSpacing(2);
+					numberOfBios.setPaintTicks(true);
+					numberOfBios.setSnapToTicks(true);
+					
+					//mainPanel.setBounds(x, y, width, height);
+					numberOfBios.setBounds(80, 70, 200, 20);
+					numOfBios.setBounds(105, 50, 500, 20);
+					mainPanel.add(numberOfBios);
+					mainPanel.add(numOfBios);
+					
+					
+					slider1l = new JLabel("Slider 1");
+					slider1 = new JSlider(JSlider.HORIZONTAL, 2, 10, 2);
+					slider1.setValue(1);
+					slider1.setMajorTickSpacing(2);
+					slider1.setPaintTicks(true);
+					
+					//mainPanel.setBounds(x, y, width, height);
+					slider1l.setBounds(150, 100, 500, 20);
+					slider1.setBounds(80, 120, 200, 20);
+					mainPanel.add(slider1l);
+					mainPanel.add(slider1);
+					
+					slider2l = new JLabel("Slider 2");
+					slider2 = new JSlider(JSlider.HORIZONTAL, 2, 10, 2);
+					slider2.setValue(1);
+					slider2.setMajorTickSpacing(2);
+					slider2.setPaintTicks(true);
+					
+					//mainPanel.setBounds(x, y, width, height);
+					slider2l.setBounds(150, 150, 500, 20);
+					slider2.setBounds(80, 170, 200, 20);
+					mainPanel.add(slider2l);
+					mainPanel.add(slider2);
+					
+					
+					numberOfBios.addChangeListener(new ChangeListener() {
+					      public void stateChanged(ChangeEvent event) {
+					       int numvalue = numberOfBios.getValue();
+					       numOfBios.setText("Number of Biomorphs " + numvalue);
+					      }
+					    });
+					
+					
+					JButton ok = new JButton("OK");
+					ok.setBounds(150, 250, 80, 20);
+					mainPanel.add(ok);
+					
+					 ok.addActionListener(new ActionListener() {
+			             @Override
+			             public void actionPerformed(ActionEvent e) {
+			             frame.dispose();
+			             }
+			         });
+					
+					
+					
+				}
+			});
+		}
 		
 		{
 			addWindowListener(new WindowListener(){
