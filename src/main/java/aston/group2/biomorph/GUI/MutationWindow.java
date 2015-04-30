6c06531139ccba538c91fff5349693556e6894a0
Theo Matthews <theo.matthews@hotmail.co.uk>
2015-04-30 13:11:49 +0100

    Buf fix: User can't hit mutate button without selecting a biomorph

--- src/main/java/aston/group2/biomorph/GUI/MutationWindow.java
+++ src/main/java/aston/group2/biomorph/GUI/MutationWindow.java
@@ -43,20 +43,19 @@ public class MutationWindow extends JFrame {
 	private int rows = 2;
 	private int cols = 3;
 
-    private static JButton makeButton(String icon, String tooltip)
-    {
-        try {
-            BufferedImage iconImg = ImageIO.read(new File("resources/icons/" + icon + ".png"));
-            JButton button = new JButton(new ImageIcon(iconImg));
-            button.setToolTipText(tooltip);
-            return button;
-        } catch (IOException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-            return null;
-        }
-    }
-
+	private static JButton makeButton(String icon, String tooltip) {
+		try {
+			BufferedImage iconImg = ImageIO.read(new File("resources/icons/"
+					+ icon + ".png"));
+			JButton button = new JButton(new ImageIcon(iconImg));
+			button.setToolTipText(tooltip);
+			return button;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return null;
+		}
+	}
 
 	public MutationWindow(int numberOfBiomorphs) {
 		this.cols = numberOfBiomorphs / rows;
@@ -76,13 +75,13 @@ public class MutationWindow extends JFrame {
 		initialiseBiomorph();
 		createHallOfFamePanel();
 		refreshGrid();
-		
+
 		JPanel header = new JPanel();
 		header.setLayout(new BorderLayout());
 		add(header, BorderLayout.NORTH);
-		
+
 		JPanel footer = new JPanel();
-		Border border = BorderFactory.createLineBorder(Color.BLACK, 1); 
+		Border border = BorderFactory.createLineBorder(Color.BLACK, 1);
 		footer.setLayout(new BorderLayout());
 		footer.setBorder(border);
 		add(footer, BorderLayout.SOUTH);
@@ -90,217 +89,230 @@ public class MutationWindow extends JFrame {
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.setLayout(new BoxLayout(buttonPanel, BoxLayout.LINE_AXIS));
 		footer.add(buttonPanel, BorderLayout.EAST);
-		
+
 		{
 			JButton refresh, undo, settings;
-			
+
 			buttonPanel.add(refresh = makeButton("table_refresh", "Refresh"));
 			buttonPanel.add(undo = makeButton("arrow_left", "Undo"));
 			buttonPanel.add(settings = makeButton("table_gear", "Settings"));
-			
+
 			settings.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					frame = new JFrame("Settings");
 					mainPanel = new JPanel();
-					
-					
-					
+
 					frame.pack();
 					frame.setVisible(true);
 					frame.setMinimumSize(new Dimension(380, 340));
 					frame.setResizable(false);
-					
+
 					mainPanel.setLayout(null);
-					
-					
+
 					frame.add(mainPanel, BorderLayout.CENTER);
-					
-					
+
 					numOfBios = new JLabel("Number of Biomorphs " + 2);
 					numberOfBios = new JSlider(JSlider.HORIZONTAL, 2, 10, 2);
 					numberOfBios.setValue(2);
 					numberOfBios.setMajorTickSpacing(2);
 					numberOfBios.setPaintTicks(true);
 					numberOfBios.setSnapToTicks(true);
-					
-					//mainPanel.setBounds(x, y, width, height);
+
+					// mainPanel.setBounds(x, y, width, height);
 					numberOfBios.setBounds(80, 70, 200, 20);
 					numOfBios.setBounds(105, 50, 500, 20);
 					mainPanel.add(numberOfBios);
 					mainPanel.add(numOfBios);
-					
-					
+
 					slider1l = new JLabel("Slider 1");
 					slider1 = new JSlider(JSlider.HORIZONTAL, 2, 10, 2);
 					slider1.setValue(1);
 					slider1.setMajorTickSpacing(2);
 					slider1.setPaintTicks(true);
-					
-					//mainPanel.setBounds(x, y, width, height);
+
+					// mainPanel.setBounds(x, y, width, height);
 					slider1l.setBounds(150, 100, 500, 20);
 					slider1.setBounds(80, 120, 200, 20);
 					mainPanel.add(slider1l);
 					mainPanel.add(slider1);
-					
+
 					slider2l = new JLabel("Slider 2");
 					slider2 = new JSlider(JSlider.HORIZONTAL, 2, 10, 2);
 					slider2.setValue(1);
 					slider2.setMajorTickSpacing(2);
 					slider2.setPaintTicks(true);
-					
-					//mainPanel.setBounds(x, y, width, height);
+
+					// mainPanel.setBounds(x, y, width, height);
 					slider2l.setBounds(150, 150, 500, 20);
 					slider2.setBounds(80, 170, 200, 20);
 					mainPanel.add(slider2l);
 					mainPanel.add(slider2);
-					
-					
+
 					numberOfBios.addChangeListener(new ChangeListener() {
-					      public void stateChanged(ChangeEvent event) {
-					       int numvalue = numberOfBios.getValue();
-					       numOfBios.setText("Number of Biomorphs " + numvalue);
-					      }
-					    });
-					
-					
+						public void stateChanged(ChangeEvent event) {
+							int numvalue = numberOfBios.getValue();
+							numOfBios
+									.setText("Number of Biomorphs " + numvalue);
+						}
+					});
+
 					JButton ok = new JButton("OK");
 					ok.setBounds(150, 250, 80, 20);
 					mainPanel.add(ok);
-					
-					 ok.addActionListener(new ActionListener() {
-			             @Override
-			             public void actionPerformed(ActionEvent e) {
-			             frame.dispose();
-			             }
-			         });
-					
-					
-					
+
+					ok.addActionListener(new ActionListener() {
+						@Override
+						public void actionPerformed(ActionEvent e) {
+							frame.dispose();
+						}
+					});
+
 				}
 			});
 		}
-		
-        addWindowListener(new WindowListener(){
 
-            @Override
-            public void windowClosing(java.awt.event.WindowEvent e) {
-                int confirm = JOptionPane.showConfirmDialog(e.getWindow(),
-                        "Are you sure you want to exit?", "Confirm",
-                        JOptionPane.OK_CANCEL_OPTION);
+		addWindowListener(new WindowListener() {
 
-                if (confirm == JOptionPane.OK_OPTION) {
-                    BiomorphHistoryLoader.save();
-                    e.getWindow().dispose();
-                }
-            }
+			@Override
+			public void windowClosing(java.awt.event.WindowEvent e) {
+				int confirm = JOptionPane.showConfirmDialog(e.getWindow(),
+						"Are you sure you want to exit?", "Confirm",
+						JOptionPane.OK_CANCEL_OPTION);
 
-            @Override public void windowActivated(java.awt.event.WindowEvent e) {}
-            @Override public void windowClosed(java.awt.event.WindowEvent e) {}
-            @Override public void windowDeactivated(java.awt.event.WindowEvent e) {}
-            @Override public void windowDeiconified(java.awt.event.WindowEvent e) {}
-            @Override public void windowIconified(java.awt.event.WindowEvent e) {}
-            @Override public void windowOpened(java.awt.event.WindowEvent e) {}
+				if (confirm == JOptionPane.OK_OPTION) {
+					BiomorphHistoryLoader.save();
+					e.getWindow().dispose();
+				}
+			}
 
-        });
+			@Override
+			public void windowActivated(java.awt.event.WindowEvent e) {
+			}
+
+			@Override
+			public void windowClosed(java.awt.event.WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeactivated(java.awt.event.WindowEvent e) {
+			}
+
+			@Override
+			public void windowDeiconified(java.awt.event.WindowEvent e) {
+			}
+
+			@Override
+			public void windowIconified(java.awt.event.WindowEvent e) {
+			}
+
+			@Override
+			public void windowOpened(java.awt.event.WindowEvent e) {
+			}
+
+		});
 
 		{
 			JButton mutateButton = new JButton("Mutate");
 			mutateButton.addActionListener(new ActionListener() {
 				@Override
 				public void actionPerformed(ActionEvent e) {
-					initialiseBiomorph();
 
-					refreshGrid();
+					boolean selected = false;
+
+					Component[] components = biomorphGrid.getComponents();
+
+					for (Component c : components) {
+						if (c instanceof BiomorphSurfaceWithTools) {
+							BiomorphSurfaceWithTools bS = (BiomorphSurfaceWithTools) c;
+
+							if (bS.selected()) {
+								selected = true;
+								break; // we only swap the first one anyway
+							}
+						}
+					}
+					if (selected) {
+						initialiseBiomorph();
+						refreshGrid();
+					}
 				}
 			});
 
 			footer.add(mutateButton, BorderLayout.CENTER);
 		}
 
-        add(biomorphGrid, BorderLayout.CENTER);
-        add(hofPanel, BorderLayout.EAST);
-        add(topOfPage, BorderLayout.NORTH);
+		add(biomorphGrid, BorderLayout.CENTER);
+		add(hofPanel, BorderLayout.EAST);
+		add(topOfPage, BorderLayout.NORTH);
 	}
 
-    private void swapClick(int slot)
-    {
-        BiomorphSurfaceWithTools selected = null;
+	private void swapClick(int slot) {
+		BiomorphSurfaceWithTools selected = null;
 
-        Component[] components = biomorphGrid.getComponents();
+		Component[] components = biomorphGrid.getComponents();
 
-        for(Component c : components)
-        {
-            if(c instanceof BiomorphSurfaceWithTools)
-            {
-                BiomorphSurfaceWithTools bS = (BiomorphSurfaceWithTools)c;
+		for (Component c : components) {
+			if (c instanceof BiomorphSurfaceWithTools) {
+				BiomorphSurfaceWithTools bS = (BiomorphSurfaceWithTools) c;
 
-                if(bS.selected())
-                {
-                    selected = bS;
-                    break; // we only swap the first one anyway
-                }
-            }
-        }
+				if (bS.selected()) {
+					selected = bS;
+					break; // we only swap the first one anyway
+				}
+			}
+		}
 
-        if(selected != null)
-        {
-            Biomorph biomorph = BiomorphHistoryLoader.hallOfFame.hallOfFame[slot];
+		if (selected != null) {
+			Biomorph biomorph = BiomorphHistoryLoader.hallOfFame.hallOfFame[slot];
 
-            if(biomorph != null)
-            {
-                Biomorph oldSelectedBM = selected.biomorphSurface.getBiomorph();
-                selected.setBiomorph(biomorph);
+			if (biomorph != null) {
+				Biomorph oldSelectedBM = selected.biomorphSurface.getBiomorph();
+				selected.setBiomorph(biomorph);
 
-                BiomorphHistoryLoader.hallOfFame.hallOfFame[slot] = oldSelectedBM;
-            }
-            else
-            {
-                BiomorphHistoryLoader.hallOfFame.hallOfFame[slot] = selected.biomorphSurface.getBiomorph();
-            }
-        }
-    }
+				BiomorphHistoryLoader.hallOfFame.hallOfFame[slot] = oldSelectedBM;
+			} else {
+				BiomorphHistoryLoader.hallOfFame.hallOfFame[slot] = selected.biomorphSurface
+						.getBiomorph();
+			}
+		}
+	}
 
-    private void initialiseBiomorph()
-    {
-        if(generation == null)
-        {
-            mutator = new Mutator();
-            mutator.childrenRequired = rows * cols;
+	private void initialiseBiomorph() {
+		if (generation == null) {
+			mutator = new Mutator();
+			mutator.childrenRequired = rows * cols;
 
-            generation = EvolutionHelper.generateSpecies(mutator).getLastestGeneration();
-        }
-        else
-        {
-            ArrayList<Biomorph> selected = new ArrayList<>(rows * cols);
+			generation = EvolutionHelper.generateSpecies(mutator)
+					.getLastestGeneration();
+		} else {
+			ArrayList<Biomorph> selected = new ArrayList<>(rows * cols);
 
-            Component[] components = biomorphGrid.getComponents();
+			Component[] components = biomorphGrid.getComponents();
 
-            for(Component c : components)
-            {
-                if(c instanceof BiomorphSurfaceWithTools)
-                {
-                    BiomorphSurfaceWithTools bS = (BiomorphSurfaceWithTools)c;
+			for (Component c : components) {
+				if (c instanceof BiomorphSurfaceWithTools) {
+					BiomorphSurfaceWithTools bS = (BiomorphSurfaceWithTools) c;
 
-                    if(bS.selected())
-                    {
-                        selected.add(bS.biomorphSurface.getBiomorph());
-                    }
-                }
-            }
+					if (bS.selected()) {
+						selected.add(bS.biomorphSurface.getBiomorph());
+					}
+				}
+			}
 
-            Biomorph[] bma = selected.toArray(new Biomorph[selected.size()]);
+			Biomorph[] bma = selected.toArray(new Biomorph[selected.size()]);
 
-            if(bma.length > 0) {
-                System.out.println(String.format("Mutating %d biomorphs", bma.length));
+			if (bma.length > 0) {
+				System.out.println(String.format("Mutating %d biomorphs",
+						bma.length));
 
-//                generation = mutator.mutateBiomorph(bma);
-                generation = EvolutionHelper.mutate(bma, mutator);
+				// generation = mutator.mutateBiomorph(bma);
+				generation = EvolutionHelper.mutate(bma, mutator);
 
-//                generation.species.printTree();
-            }
-        }
-    }
+				// generation.species.printTree();
+			}
+		}
+	}
 
 	private void refreshGrid() {
 		biomorphGrid.removeAll();
@@ -309,99 +321,96 @@ public class MutationWindow extends JFrame {
 			BiomorphSurfaceWithTools bs = new BiomorphSurfaceWithTools(true);
 			bs.setBiomorph(generation.children[i]);
 			biomorphGrid.add(bs);
-			
+
 		}
 
 		biomorphGrid.revalidate();
 	}
 
-    private JComponent createHallOfFameSlot(int i)
-    {
-        JComponent biomorph = (BiomorphHistoryLoader.hallOfFame.hallOfFame[i] != null ?
-                new BiomorphSurface(BiomorphHistoryLoader.hallOfFame.hallOfFame[i]) :
-                new JLabel("None") );
+	private JComponent createHallOfFameSlot(int i) {
+		JComponent biomorph = (BiomorphHistoryLoader.hallOfFame.hallOfFame[i] != null ? new BiomorphSurface(
+				BiomorphHistoryLoader.hallOfFame.hallOfFame[i]) : new JLabel(
+				"None"));
 
-        return biomorph;
-    }
+		return biomorph;
+	}
 
 	private void createHallOfFamePanel() {
-        if(hofPanel == null) {
-            hofPanel = new JPanel();
-            hofPanel.setLayout(new GridBagLayout());
-            hofPanel.setPreferredSize(new Dimension(150,0));
-            hofPanel.setBorder(BorderFactory.createEmptyBorder(10,0,10,10));
-        }
-        else {
-            hofPanel.removeAll();
-        }
+		if (hofPanel == null) {
+			hofPanel = new JPanel();
+			hofPanel.setLayout(new GridBagLayout());
+			hofPanel.setPreferredSize(new Dimension(150, 0));
+			hofPanel.setBorder(BorderFactory.createEmptyBorder(10, 0, 10, 10));
+		} else {
+			hofPanel.removeAll();
+		}
 
-        for(int i=0; i<BiomorphHistoryLoader.hallOfFame.hallOfFame.length; i++)
-        {
-            final JPanel biomorphHolder = new JPanel();
-            biomorphHolder.setLayout(new BorderLayout());
-            biomorphHolder.add(createHallOfFameSlot(i));
+		for (int i = 0; i < BiomorphHistoryLoader.hallOfFame.hallOfFame.length; i++) {
+			final JPanel biomorphHolder = new JPanel();
+			biomorphHolder.setLayout(new BorderLayout());
+			biomorphHolder.add(createHallOfFameSlot(i));
 
-//            biomorph.setPreferredSize(new Dimension(80, 60));
-            biomorphHolder.setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+			// biomorph.setPreferredSize(new Dimension(80, 60));
+			biomorphHolder.setBorder(BorderFactory.createLineBorder(
+					Color.BLACK, 1));
 
-            JButton swap = makeButton("arrow_switch", "Swap");
-            JButton clear = makeButton("cancel", "Clear");
+			JButton swap = makeButton("arrow_switch", "Swap");
+			JButton clear = makeButton("cancel", "Clear");
 
-            swap.addActionListener(new ActionListener() {
-                private int slot;
+			swap.addActionListener(new ActionListener() {
+				private int slot;
 
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    swapClick(slot);
-                    biomorphHolder.removeAll();
-                    biomorphHolder.add(createHallOfFameSlot(slot));
-                    biomorphHolder.revalidate();
-                }
+				@Override
+				public void actionPerformed(ActionEvent e) {
+					swapClick(slot);
+					biomorphHolder.removeAll();
+					biomorphHolder.add(createHallOfFameSlot(slot));
+					biomorphHolder.revalidate();
+				}
 
-                public ActionListener init(int slot)
-                {
-                    this.slot = slot;
-                    return this;
-                }
-            }.init(i));
+				public ActionListener init(int slot) {
+					this.slot = slot;
+					return this;
+				}
+			}.init(i));
 
-            {
-                Dimension d = new Dimension(22,22);
-                swap.setPreferredSize(d);
-                clear.setPreferredSize(d);
-            }
+			{
+				Dimension d = new Dimension(22, 22);
+				swap.setPreferredSize(d);
+				clear.setPreferredSize(d);
+			}
 
-            GridBagConstraints c = new GridBagConstraints();
+			GridBagConstraints c = new GridBagConstraints();
 
-            Insets is = new Insets(5, 10, 0, 0);
+			Insets is = new Insets(5, 10, 0, 0);
 
-            c.insets = is;
+			c.insets = is;
 
-            {
-                c.fill = GridBagConstraints.BOTH;
+			{
+				c.fill = GridBagConstraints.BOTH;
 
-                c.gridx = 0;
-                c.gridy = 2*i;
-                c.gridheight = 2;
-                c.weightx = 1;
+				c.gridx = 0;
+				c.gridy = 2 * i;
+				c.gridheight = 2;
+				c.weightx = 1;
 
-                hofPanel.add(biomorphHolder, c);
-            }
+				hofPanel.add(biomorphHolder, c);
+			}
 
-            {
-                c.fill = GridBagConstraints.NONE;
+			{
+				c.fill = GridBagConstraints.NONE;
 
-                c.gridx = 1;
-                c.gridy = 2*i;
-                c.gridheight = 1;
-                c.weightx = 0;
-                c.weighty = 0.5;
+				c.gridx = 1;
+				c.gridy = 2 * i;
+				c.gridheight = 1;
+				c.weightx = 0;
+				c.weighty = 0.5;
 
-                hofPanel.add(swap, c);
+				hofPanel.add(swap, c);
 
-                c.gridy = 2*i + 1;
-                hofPanel.add(clear, c);
-            }
-        }
-	}	
+				c.gridy = 2 * i + 1;
+				hofPanel.add(clear, c);
+			}
+		}
+	}
 }
